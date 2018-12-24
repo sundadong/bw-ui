33773936174a20875136cffa0e05f63064ac4cd7
sundadong <948088101@qq.com>
2018-12-24 13:22:55 +0800

    init

---
+++ src/example/components.js
@@ -0,0 +1,118 @@
+export default [
+  {
+    name: '基础',
+    id: 'basic',
+    children: [
+      {
+        name: '图标',
+        id: 'icon'
+      },
+      {
+        name: '按钮',
+        id: 'button'
+      }
+    ]
+  },
+  {
+    name: '视图',
+    id: 'view',
+    children: [
+      {
+        name: '头像',
+        id: 'avatar'
+      },
+      {
+        name: '文章',
+        id: 'article'
+      },
+      {
+        name: '徽标',
+        id: 'badge'
+      },
+      {
+        name: '标签',
+        id: 'tag'
+      },
+      {
+        name: '提示框',
+        id: 'alert'
+      },
+      {
+        name: '倒计时',
+        id: 'countdown'
+      },
+      {
+        name: '空状态',
+        id: 'empty'
+      },
+      {
+        name: '页面提示',
+        id: 'loadmore'
+      },
+      {
+        name: '时间轴',
+        id: 'timeline'
+      }
+    ]
+  },
+  {
+    name: '操作反馈',
+    id: 'feedback',
+    children: [
+      {
+        name: '弹出层',
+        id: 'popup'
+      },
+      {
+        name: '加载中',
+        id: 'loading'
+      },
+      {
+        name: '消息通知',
+        id: 'notify'
+      }
+    ]
+  },
+  {
+    name: '表单',
+    id: 'form',
+    children: [
+      {
+        name: '输入框',
+        id: 'input'
+      },
+      {
+        name: '多行文本框',
+        id: 'textarea'
+      },
+      {
+        name: '步进器',
+        id: 'stepper'
+      }
+    ]
+  },
+  {
+    name: '布局',
+    id: 'layout',
+    children: [
+      {
+        name: '栅格布局',
+        id: 'layout'
+      },
+      {
+        name: '列表',
+        id: 'cell'
+      }
+    ]
+  },
+  {
+    name: '导航',
+    id: 'nav',
+    children: [
+      {
+        name: '标签页',
+        id: 'tabs'
+      }
+    ]
+  }
+]
