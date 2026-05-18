6d891e8648cc9301f0a69fb40913f99b8df5e531
sundadong <sundadong@users.noreply.github.com>
2026-05-18 13:49:55 +0000

    feat: 项目概述
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- bwui/src/main.ts
+++ bwui/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/common.scss'
+import '@vant/icons/src/index.less'
 
 const app = createApp(App)
 app.use(router)
